c73c8a2c35c973cea43860bb4bd3a38e02199cc6
darius-gheorghe <darghe98@gmail.com>
2024-08-01 18:34:03 +0300

    Removed unused plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    //alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.dagger.hilt.android")
     id("kotlin-kapt")
     kotlin("android")
